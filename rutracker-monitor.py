63af636acda1e906e464c3afe2bfe25427492d6d
ZSerg <zsergt3@gmail.com>
2017-10-16 20:40:46 +0300

    ...

--- rutracker-monitor.py
+++ rutracker-monitor.py
@@ -22,6 +22,7 @@ bot_url = "https://api.telegram.org/bot%s/"%TOKEN
 
 def get_updates_json(request):
     #import pdb; pdb.set_trace()
+    print("get_updates");
     response = requests.get(request + 'getUpdates')
     return response.json()
 
@@ -36,11 +37,13 @@ def get_chat_id(update):
     return chat_id
 
 def send_mess(chat, text):
+    print("send_mess");
     params = {'chat_id': chat, 'text': text, 'parse_mode':'HTML'}
     response = requests.post(bot_url + 'sendMessage', data=params)
     return response
 
 def send_image(chat, text):
+    print("send_image");
     params = {'chat_id': chat, 'photo': text}
     response = requests.post(bot_url + 'sendPhoto', data=params)
     return response
@@ -57,46 +60,56 @@ def get_poster_url(tag):
 
 last_check_time = datetime.now().timestamp()
 
-with requests.Session() as s:
-    resp = s.post(login_url, data=payload)
+while True:
+    try:
+        with requests.Session() as s:
+            resp = s.post(login_url, data=payload)
 
-    while True:
-        time.sleep(UPDATE_PERIOD)
-        now = datetime.now()
-        print(now)
-        if now.hour >= SILENT_PERIOD[0] or now.hour < SILENT_PERIOD[1]:
-            print("Silent period")
-        else:
-            for url in urls:
-                resp = s.post(url)
+            while True:
+                time.sleep(UPDATE_PERIOD)
+                now = datetime.now()
+                print(now)
+                if now.hour >= SILENT_PERIOD[0] or now.hour < SILENT_PERIOD[1]:
+                    print("Silent period")
+                else:
+                    for url in urls:
+                        print("while post");
+                        resp = s.post(url)
 
-                soup = BeautifulSoup(resp.text, 'html.parser')
-                rows = soup.select('#tor-tbl tr')
+                        soup = BeautifulSoup(resp.text, 'html.parser')
+                        rows = soup.select('#tor-tbl tr')
 
-                for row in rows:
-                    try:
-                        name = row.select('.tLink')[0]
-                        # img_url = get_poster_url(name)
-                        # chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
-                        # send_image(chat_id, img_url)
+                        for row in rows:
+                            try:
+                                name = row.select('.tLink')[0]
+                                # img_url = get_poster_url(name)
+                                # chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
+                                # send_image(chat_id, img_url)
 
-                        name = str(name).replace('viewtopic', 'https://rutracker.net/forum/viewtopic')
-                        timestamp = int(row.select('td')[-1].select('u')[0].contents[0])
+                                name = str(name).replace('viewtopic', 'https://rutracker.net/forum/viewtopic')
+                                timestamp = int(row.select('td')[-1].select('u')[0].contents[0])
 
-                        # chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
-                        # send_mess(chat_id, name)
+                                # chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
+                                # send_mess(chat_id, name)
 
-                    except IndexError:
-                        pass
-                    else:
-                        if timestamp > last_check_time:
-                            print(timestamp, name)
-                            chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
-                            send_mess(chat_id, name)
-                        # else:
-                        #     chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
-                        #     send_mess(chat_id, '[inline URL](http://www.example.com/)')
+                            except IndexError:
+                                pass
+                            else:
+                                if timestamp > last_check_time:
+                                    print(timestamp, name)
+                                    try:
+                                        chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
+                                    except:
+                                        print("Get chat_id exception")
+                                        pass
+                                    else:
+                                        send_mess(chat_id, name)
+                                # else:
+                                #     chat_id = get_chat_id(last_update(get_updates_json(bot_url)))
+                                #     send_mess(chat_id, '[inline URL](http://www.example.com/)')
 
-            last_check_time = datetime.now().timestamp()
+                    last_check_time = datetime.now().timestamp()
+    except:
+        print("Top exception")
 
 
